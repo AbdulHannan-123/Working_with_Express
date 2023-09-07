52e0df5a0aece877be8060fc037be9f76adfc95f
AbdulHannan-123 <ah6475774@gmail.com>
2023-09-08 00:50:14 +0500

    exploring express js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express'); 
 
@@ -6,13 +6,17 @@ const app = express();  // the above exress exports a funtion here, so we use it
 
 app.use((req, res, next) => {  // it allow us ko add a middleware func
     console.log('In the middleware!');
+    
     next(); // allow the request to continue to the next middleware in line
+    res.send('<h1>Hello from express1!</h1>');
 });  
 
 app.use((req, res, next) => {
     console.log('In another middleware!');
+    res.send('<h1>Hello from express!</h1>');
 }); 
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// server.listen(3000);
 
-server.listen(3000);
+app.listen(3000); // this and above 2 lines do the same thing
