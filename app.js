6cf01e53a9a292ee7d03977bc8d7436d7bf02635
AbdulHannan-123 <ah6475774@gmail.com>
2023-09-11 19:09:37 +0500

    getting and parsing data

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-// const http = require('http');
-
 const express = require('express'); 
+const bodyParse = require('body-parser')
 
-const app = express();  // the above exress exports a funtion here, so we use it with app variable
+const app = express();  // the above exress exports a funtion here, so we use it with app variable 
 
-app.use((req, res, next) => {  // it allow us ko add a middleware func
-    console.log('In the middleware!');
-    
-    next(); // allow the request to continue to the next middleware in line
-    res.send('<h1>Hello from express1!</h1>');
-});  
+app.use(bodyParse.urlencoded({extendedfasle})); // only parse body getting from the form  like in our case
 
-app.use((req, res, next) => {
+
+
+app.use("/add-products",(req, res, next) => {
     console.log('In another middleware!');
-    res.send('<h1>Hello from express!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type= "submit">Add product</button></form>');
+}); 
+
+app.use("/product",(req, res) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+app.use("/",(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express/</h1>');
 }); 
 
 // const server = http.createServer(app);
