2002dce8437f7881f5ad9a9ae2f2270485e7ab9b
AbdulHannan-123 <ah6475774@gmail.com>
2023-09-18 19:13:15 +0500

    1)saperates routes 2)added the not fond page 3)adding the headertou more filter the path

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express'); 
 const bodyParse = require('body-parser')
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();  // the above exress exports a funtion here, so we use it with app variable 
 
-app.use(bodyParse.urlencoded({extendedfasle})); // only parse body getting from the form  like in our case
+app.use(bodyParse.urlencoded({extended: false})); // only parse body getting from the form  like in our case
 
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-app.use("/add-products",(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type= "submit">Add product</button></form>');
-}); 
-
-app.use("/product",(req, res) => {
-    console.log(req.body);
-    res.redirect("/");
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 });
-app.use("/",(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from express/</h1>');
-}); 
 
 // const server = http.createServer(app);
 // server.listen(3000);
