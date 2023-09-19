7e62897a42e512a596d4eba2500c042a8ed0c5ee
AbdulHannan-123 <ah6475774@gmail.com>
2023-09-20 00:53:59 +0500

    1)added the path 2) make that in MVC pattern 3) implemented the path

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express'); 
 const bodyParse = require('body-parser')
 
@@ -8,11 +9,13 @@ const app = express();  // the above exress exports a funtion here, so we use it
 
 app.use(bodyParse.urlencoded({extended: false})); // only parse body getting from the form  like in our case
 
-app.use(adminRoutes);
+/// suppose all the paths having the same path route then we extract the header from all of them
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))
+    // res.status(404).send('<h1>Page not found </h1>')
 });
 
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
-
+/// this path becoms /admin/add-prodcts
 router.get("/add-products",(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type= "submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    // res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type= "submit">Add product</button></form>');
 }); 
-
+/// this pah become /admin/product
 router.post("/product",(req, res) => {
     console.log(req.body);
     res.redirect("/");
